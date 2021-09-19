2f6246739c8ba0053077a8a4a30e7b47313a4d9e
binisg <85735940+binisg@users.noreply.github.com>
2021-09-19 23:34:05 +0300

    Just a Little Honest Mistake Fix

--- script.js
+++ script.js
@@ -6,17 +6,6 @@ const esc = document.getElementById('btnesc');
 const add = document.getElementById('btnplus');
 let acceptingNumber = true;
 
-function col() { // When the user clicks the element,
-  if (acceptingNumber === false) return;
-  var currentNumber = this.textContent; // take the element's number,
-  num1.push(currentNumber); // and add it to the 'num1' array.
-  result.textContent = String(num1.join('')); // Then take the result's text content and add to it the element's number.
-  if (num1.join('') == '00') { // If the users presses double zeros,
-    num1 = []; // delete all the array items,
-    result.textContent = '0'; // and show '0'.
-  };
-}
-
 function firstNumber() {
   for (i of nmbr) {
       i.addEventListener('click', function() { // When the user clicks the element,
