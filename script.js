33a92f2cf71c1a666a4803266bd357246d33d8d3
binisg <binisgiannis@gmail.com>
2021-09-19 19:04:25 +0300

    Added some Comments

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let calculation = [];
+let num1 = [];
 let result = document.getElementById('btnresult');
 let nmbr = document.getElementsByClassName('btn');
 const del = document.getElementById('btndel');
@@ -8,29 +8,29 @@ const add = document.getElementById('btnplus');
 result.textContent = '0';
 
 for (i of nmbr) {
-    i.addEventListener('click', function() {
-      var currentNumber = this.textContent;
-      calculation.push(currentNumber);
-      result.textContent = String(calculation.join(''));
-      if (calculation.join('') == '00') {
-        calculation = [];
-        result.textContent = '0';
+    i.addEventListener('click', function() { // When the user clicks the element,
+      var currentNumber = this.textContent; // take the element's number,
+      num1.push(currentNumber); // and add it to the 'num1' array.
+      result.textContent = String(num1.join('')); // Then take the result's text content and add to it the element's number.
+      if (num1.join('') == '00') { // If the users presses double zeros,
+        num1 = []; // delete all the array items,
+        result.textContent = '0'; // and show '0'.
       };
     });
 }
 
 del.addEventListener('click', function() {
-    calculation.splice(-1, 1);
-    result.textContent = String(calculation.join(''));
-    if (calculation.join('') == '') result.textContent = '0';
+    num1.splice(-1, 1);
+    result.textContent = String(num1.join(''));
+    if (num1.join('') == '') result.textContent = '0';
 })
 
 esc.addEventListener('click', function() {
     result.textContent = '0';
-    calculation = [];
+    num1 = [];
 })
 
 add.addEventListener('click', function() {
   result.textContent += ' ' + '+' + ' ';
-  
+  // somehow make the 'num1' array stop receiving any further numbers.
 })
