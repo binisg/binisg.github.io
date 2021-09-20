1dd0e1eabf2e4e90ed0bc49a9f1abc42ebabd370
Giannis Binis <binisgiannis@gmail.com>
2021-09-20 14:56:28 +0300

    separating the 'access' part

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 let num1 = [];
+let num2 = [];
 let result = document.getElementById('btnresult');
 let nmbr = document.getElementsByClassName('btn');
-const del = document.getElementById('btndel');
-const esc = document.getElementById('btnesc');
 const add = document.getElementById('btnplus');
-let acceptingNumber = true;
+const eql = document.getElementById('btnequal')
 
 function firstNumber() {
   for (i of nmbr) {
@@ -21,34 +20,35 @@ function firstNumber() {
   }
 }
 
-firstNumber();
-
-function acceptingDigits() {
-  if (result.textContent.charAt(result.textContent.length-1) !== ' ') {
-    acceptingNumber = true;
+function secondNumber() {
+  for (i of nmbr) {
+      i.addEventListener('click', function() { // When the user clicks the element,
+        if (acceptNumber === false) return;
+        var currentNumber = this.textContent; // take the element's number,
+        num2.push(currentNumber); // and add it to the 'num1' array.
+        result.textContent += String(currentNumber) // Then take the result's text content and add to it the element's number.
+        if (num2.join('') == '00') { // If the users presses double zeros,
+          num2 = []; // delete all the array items,
+          result.textContent += '0'; // and show '0'.
+        };
+      });
   }
 }
 
-del.addEventListener('click', function() {
-  if (result.textContent.charAt(result.textContent.length-1) !== ' ') num1.splice(-1, 1);
-  else result.textContent.slice(0, -3);
-  result.textContent = String(num1.join(''));
-  if (num1.join('') == '') result.textContent = '0';
-  acceptingDigits();
-})
-
-esc.addEventListener('click', function() {
-    result.textContent = '0';
-    num1 = [];
-    acceptingDigits();
-})
-
-function testing() {
-  result.textContent += ' ' + '+' + ' ';
-  acceptingNumber = false;
-}
+firstNumber();
 
 add.addEventListener('click', function() {
   result.textContent += ' ' + '+' + ' ';
-  acceptingNumber = false;
+  falsing();
+  if (result.textContent.charAt(result.textContent.length-3) == ' ' && result.textContent.charAt(result.textContent.length-4) == ' ') {
+    result.textContent = result.textContent.substring(0, result.textContent.length - 3);
+  }
+  secondNumber();
+})
+eql.addEventListener('click', function() {
+  result.textContent += ' ' + '=' + ' ';
+  falseing();
+  if (result.textContent.charAt(result.textContent.length-3) == ' ' && result.textContent.charAt(result.textContent.length-4) == ' ') {
+    result.textContent = result.textContent.substring(0, result.textContent.length - 3);
+  }
 })
