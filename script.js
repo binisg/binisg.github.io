ebf0f9b9adfb6a3d8ac2fde217c7ac87337eb9fa
Giannis Binis <binisgiannis@gmail.com>
2021-09-20 15:20:18 +0300

    Fixed the 'acceptingDigits()' function

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ let num1 = [];
 let num2 = [];
 let result = document.getElementById('btnresult');
 let nmbr = document.getElementsByClassName('btn');
+const del = document.getElementById('btndel');
+const esc = document.getElementById('btnesc');
 const add = document.getElementById('btnplus');
 const eql = document.getElementById('btnequal')
+let acceptingNumber = true;
+let acceptNumber = true;
 
 function firstNumber() {
   for (i of nmbr) {
@@ -37,17 +41,37 @@ function secondNumber() {
 
 firstNumber();
 
+function acceptingDigits() {
+  if (result.textContent.charAt(result.textContent.length-1) !== ' ' && acceptNumber === false) {
+    acceptingNumber = true;
+  }
+}
+
+del.addEventListener('click', function() {
+  if (result.textContent.charAt(result.textContent.length-1) !== ' ') result.textContent = result.textContent.substring(0, result.textContent.length - 1);
+  else result.textContent = result.textContent.substring(0, result.textContent.length - 3);
+  if (result.textContent == '') result.textContent = '0';
+  acceptingDigits();
+})
+
+esc.addEventListener('click', function() {
+    result.textContent = '0';
+    num1 = [];
+    acceptingDigits();
+})
+
 add.addEventListener('click', function() {
   result.textContent += ' ' + '+' + ' ';
-  falsing();
+  acceptingNumber = false;
   if (result.textContent.charAt(result.textContent.length-3) == ' ' && result.textContent.charAt(result.textContent.length-4) == ' ') {
     result.textContent = result.textContent.substring(0, result.textContent.length - 3);
   }
   secondNumber();
 })
+
 eql.addEventListener('click', function() {
   result.textContent += ' ' + '=' + ' ';
-  falseing();
+  acceptNumber = false;
   if (result.textContent.charAt(result.textContent.length-3) == ' ' && result.textContent.charAt(result.textContent.length-4) == ' ') {
     result.textContent = result.textContent.substring(0, result.textContent.length - 3);
   }
