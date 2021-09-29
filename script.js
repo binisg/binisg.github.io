01bed9c9e79428b1f07bcd2de9d03b834002f12a
Giannis Binis <binisgiannis@gmail.com>
2021-09-29 15:08:25 +0300

    YES! FINALLY! I Solved It! (i think)

--- script.js
+++ script.js
@@ -128,42 +128,38 @@ divide.addEventListener('click', function() {
 function calculate(op) {
   switch (op) {
     case 'Times':
-      var x = finalNmbrs.lastIndexOf('x');
+      var x = finalNmbrs.indexOf('x');
       var prev = finalNmbrs[x-1];
       var aft = finalNmbrs[x+1];
       var calc = Number(prev) * Number(aft);
-      finalNmbrs.splice(finalNmbrs.lastIndexOf(prev), 3, calc);
+      finalNmbrs.splice(finalNmbrs.indexOf(prev), 3, calc);
       op3--;
       break;
 
       case 'Divide':
-      var y = finalNmbrs.lastIndexOf(':');
+      var y = finalNmbrs.indexOf(':');
       var prev = finalNmbrs[y-1];
       var aft = finalNmbrs[y+1];
       var calc = Number(prev) / Number(aft);
-      finalNmbrs.splice(finalNmbrs.lastIndexOf(prev), 3, calc);
+      finalNmbrs.splice(finalNmbrs.indexOf(prev), 3, calc);
       op4--;
       break;
       
-      case 'Plus':
-      var z = finalNmbrs.lastIndexOf('+');
+  case 'Plus':
+      var z = finalNmbrs.indexOf('+');
       var prev = finalNmbrs[z-1];
       var aft = finalNmbrs[z+1];
       var calc = Number(prev) + Number(aft);
-      console.log(finalNmbrs);
-      finalNmbrs.splice(finalNmbrs.lastIndexOf(prev), 3, calc);
-      console.log(finalNmbrs);
+      finalNmbrs.splice(finalNmbrs.indexOf(prev), 3, calc);
       op1--;
       break;
       
-      case 'Minus':
-      var e = finalNmbrs.lastIndexOf('-');
+  case 'Minus':
+      var e = finalNmbrs.indexOf('-');
       var prev = finalNmbrs[e-1];
       var aft = finalNmbrs[e+1];
       var calc = Number(prev) - Number(aft);
-      console.log(finalNmbrs);
-      finalNmbrs.splice(finalNmbrs.lastIndexOf(prev), 3, calc);
-      console.log(finalNmbrs);
+      finalNmbrs.splice(finalNmbrs.indexOf(prev), 3, calc);
       op2--;
       break;
   }
@@ -180,23 +176,39 @@ eql.addEventListener('click', function() {
     if (finalNmbrs[i] === ':') op4++;
   };
 
+  // The following loops need serious fixing (!).
+  // Everything but the first operation of the 'while'
+  // loop runs smoothly. But the first operation,
+  // for some reason, does a random calculation and
+  // messes up the array. Also, my poor mind just 
+  // discovered that, in one calculation, if the
+  // addition performs first and then the substraction,
+  // is different than if the substraction was to perform
+  // before the addition. In my brain, for some reason,
+  // this was not 100% clear at the time. Last but not
+  // least, when (God first) I finish with this tiring,
+  // exhausting 'calculate' function, I ought to fix other
+  // bugs, such as the ability of a user to press '0'
+  // multiple times. I don't want that. Finally, I also
+  // don't want a user to have the ability to press one
+  // operation symbol after another. Good night.
 
-  if (op3 !== 0 && op4 == 0) {
+  if (op3 !== 0) {
     while (op3 !== 0) {
       calculate('Times');
     }
   }
-  if (op4 !== 0 && op3 == 0) {
+  if (op4 !== 0) {
     while (op4 !== 0) {
       calculate('Divide');
     }
   }
-  if (op1 !== 0 && op2 == 0) {
+  if (op1 !== 0) {
     while (op1 !== 0) {
       calculate('Plus');
     }
   }
-  if (op2 !== 0 && op1 == 0) {
+  if (op2 !== 0) {
     while (op2 !== 0) {
       calculate('Minus');
     }
